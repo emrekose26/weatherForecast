b278cbe09765d9ff6ee5f5eabb4c370d2e13ca46
Emre Köse <e.kose26@gmail.com>
2016-08-09 17:23:15 +0300

    Fragments design and api request

--- app/src/main/java/com/emrekose/weatherapp/fragments/WeatherOneFragment.java
+++ app/src/main/java/com/emrekose/weatherapp/fragments/WeatherOneFragment.java
@@ -1,27 +1,35 @@
 package com.emrekose.weatherapp.fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.emrekose.weatherapp.R;
+import com.emrekose.weatherapp.model.ForecastResponse;
+import com.emrekose.weatherapp.rest.ApiClient;
+import com.emrekose.weatherapp.rest.ApiInterface;
+import com.emrekose.weatherapp.utils.Constants;
+import com.emrekose.weatherapp.utils.DateUtils;
+import com.emrekose.weatherapp.utils.WeatherCalc;
+import com.emrekose.weatherapp.utils.WeatherUtils;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class WeatherOneFragment extends Fragment {
 
+    private TextView tempFragmentTextView1, tempFragmentTextView2 ,dayOneTextView, dayTwoTextView;
+    private ImageView fragment1ImageView, fragment2ImageView;
+
     public WeatherOneFragment() {
         // Required empty public constructor
-    }
-
-    public static WeatherOneFragment newInstance(String param1, String param2) {
-        WeatherOneFragment fragment = new WeatherOneFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
@@ -33,11 +41,42 @@ public class WeatherOneFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather_one, container, false);
+        View view =  inflater.inflate(R.layout.fragment_weather_one, container, false);
+        tempFragmentTextView1 = (TextView)view.findViewById(R.id.tempFragmentTextView1);
+        tempFragmentTextView2 = (TextView)view.findViewById(R.id.tempFragmentTextView2);
+
+        dayOneTextView = (TextView)view.findViewById(R.id.dayOneTextView);
+        dayTwoTextView = (TextView)view.findViewById(R.id.dayTwoTextView);
+
+        fragment1ImageView = (ImageView)view.findViewById(R.id.fragment1ImageView);
+        fragment2ImageView = (ImageView)view.findViewById(R.id.fragment2ImageView);
+
+        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+
+        Call <ForecastResponse> forecastResponseCall = apiService.getFiveDaysDatas("eskisehir", Constants.API_KEY);
+        forecastResponseCall.enqueue(new Callback<ForecastResponse>() {
+            @Override
+            public void onResponse(Call<ForecastResponse> call, Response<ForecastResponse> response) {
+                tempFragmentTextView1.setText(WeatherCalc.kelvinToCelcius(response.body().getLists().get(9).getForecastMain().getTemp()));
+                dayOneTextView.setText(DateUtils.getDayName(response.body().getLists().get(9).getDtTxt()));
+
+                tempFragmentTextView2.setText(WeatherCalc.kelvinToCelcius(response.body().getLists().get(17).getForecastMain().getTemp()));
+                dayTwoTextView.setText(DateUtils.getDayName(response.body().getLists().get(17).getDtTxt()));
+
+                WeatherUtils.setFragmentWeatherIcon(fragment1ImageView,response.body().getLists().get(9).getForecastWeatherList().get(0).getIcon());
+                WeatherUtils.setFragmentWeatherIcon(fragment2ImageView,response.body().getLists().get(17).getForecastWeatherList().get(0).getIcon());
+            }
+
+            @Override
+            public void onFailure(Call<ForecastResponse> call, Throwable t) {
+                Log.e("FRAGMENT_ERROR", "onFailure: " + t.toString() );
+            }
+        });
+
+        return view;
     }
 
-    public void onButtonPressed(Uri uri) {
 
-    }
+
 
 }
--- app/src/main/java/com/emrekose/weatherapp/fragments/WeatherTwoFragment.java
+++ app/src/main/java/com/emrekose/weatherapp/fragments/WeatherTwoFragment.java
@@ -3,16 +3,33 @@ package com.emrekose.weatherapp.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.emrekose.weatherapp.R;
+import com.emrekose.weatherapp.model.ForecastResponse;
+import com.emrekose.weatherapp.rest.ApiClient;
+import com.emrekose.weatherapp.rest.ApiInterface;
+import com.emrekose.weatherapp.utils.Constants;
+import com.emrekose.weatherapp.utils.DateUtils;
+import com.emrekose.weatherapp.utils.WeatherCalc;
+import com.emrekose.weatherapp.utils.WeatherUtils;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WeatherTwoFragment extends Fragment {
+
+    private TextView tempFragmentTextView3, tempFragmentTextView4 ,dayThreeTextView, dayFourTextView;
+    private ImageView fragment3ImageView, fragment4ImageView;
 
 
     public WeatherTwoFragment() {
@@ -24,7 +41,40 @@ public class WeatherTwoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather_two, container, false);
+        View view = inflater.inflate(R.layout.fragment_weather_two, container, false);
+
+        tempFragmentTextView3 = (TextView)view.findViewById(R.id.tempFragmentTextView3);
+        tempFragmentTextView4 = (TextView)view.findViewById(R.id.tempFragmentTextView4);
+
+        dayThreeTextView = (TextView)view.findViewById(R.id.dayThreeTextView);
+        dayFourTextView = (TextView)view.findViewById(R.id.dayFourTextView);
+
+        fragment3ImageView = (ImageView)view.findViewById(R.id.fragment3ImageView);
+        fragment4ImageView = (ImageView)view.findViewById(R.id.fragment4ImageView);
+
+        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+
+        Call<ForecastResponse> forecastResponseCall = apiService.getFiveDaysDatas("eskisehir", Constants.API_KEY);
+        forecastResponseCall.enqueue(new Callback<ForecastResponse>() {
+            @Override
+            public void onResponse(Call<ForecastResponse> call, Response<ForecastResponse> response) {
+                tempFragmentTextView3.setText(WeatherCalc.kelvinToCelcius(response.body().getLists().get(25).getForecastMain().getTemp()));
+                dayThreeTextView.setText(DateUtils.getDayName(response.body().getLists().get(25).getDtTxt()));
+
+                tempFragmentTextView4.setText(WeatherCalc.kelvinToCelcius(response.body().getLists().get(33).getForecastMain().getTemp()));
+                dayFourTextView.setText(DateUtils.getDayName(response.body().getLists().get(33).getDtTxt()));
+
+                WeatherUtils.setFragmentWeatherIcon(fragment3ImageView,response.body().getLists().get(25).getForecastWeatherList().get(0).getIcon());
+                WeatherUtils.setFragmentWeatherIcon(fragment4ImageView,response.body().getLists().get(33).getForecastWeatherList().get(0).getIcon());
+            }
+
+            @Override
+            public void onFailure(Call<ForecastResponse> call, Throwable t) {
+                Log.e("FRAGMENT_ERROR", "onFailure: " + t.toString() );
+            }
+        });
+
+        return view;
     }
 
 }
