25ac37e337463af0bb77eb997af05177fbcc51e7
Emre Köse <e.kose26@gmail.com>
2016-08-09 17:23:49 +0300

    new utils added

---
+++ app/src/main/java/com/emrekose/weatherapp/utils/DateUtils.java
@@ -0,0 +1,24 @@
+package com.emrekose.weatherapp.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by emrekose on 9.08.2016.
+ */
+
+public class DateUtils {
+    public static String getDayName(String input){
+
+        SimpleDateFormat inFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = null;
+        try {
+            date = inFormat.parse(input);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        SimpleDateFormat outFormat = new SimpleDateFormat("EEEE");
+        return outFormat.format(date);
+    }
+}
--- app/src/main/java/com/emrekose/weatherapp/utils/WeatherUtils.java
+++ app/src/main/java/com/emrekose/weatherapp/utils/WeatherUtils.java
@@ -33,5 +33,47 @@ public class WeatherUtils {
         }
     }
 
+    public static void setFragmentWeatherIcon(ImageView imageView, String iconName){
+
+        switch (iconName){
+            case "01d" :
+            case "01n" : imageView.setImageResource(R.drawable.fdayzeroone);
+                break;
+
+            case "02d" :
+            case "02n" : imageView.setImageResource(R.drawable.fdayzerotwo);
+                break;
+
+            case "03d" :
+            case "03n" : imageView.setImageResource(R.drawable.fdayzerothree);
+                break;
+
+            case "04d" :
+            case "04n" : imageView.setImageResource(R.drawable.fdayzerofour);
+                break;
+
+            case "09d" :
+            case "09n" : imageView.setImageResource(R.drawable.fdayzeronine);
+                break;
+
+            case "10d" :
+            case "10n" : imageView.setImageResource(R.drawable.fdayonezero);
+                break;
+
+            case "11d" :
+            case "11n" : imageView.setImageResource(R.drawable.fdayoneone);
+                break;
+
+            case "13d" :
+            case "13n" : imageView.setImageResource(R.drawable.fdayonethree);
+                break;
+
+            case "50d" :
+            case "50n" : imageView.setImageResource(R.drawable.fdayfivezero);
+                break;
+        }
+
+    }
+
     
 }
